a7c7ad41b5fdac595b6f4cda1cf585736d01390d
Gustavo <gustavo.ferreira@kinvo.com.br>
2023-09-15 18:14:53 -0300

    feature: add people initial setup

--- fintech-challenge/Database/DatabaseContext.cs
+++ fintech-challenge/Database/DatabaseContext.cs
@@ -1,9 +1,12 @@
+using FintechChallenge.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace FintechChallenge.Database
 {
     public class DatabaseContext : DbContext
     {
+
+        public DbSet<People> People { get; set; }
 
         public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
         {
---
+++ fintech-challenge/Migrations/20230915210301_create-people-table.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace fintechchallenge.Migrations
+{
+    /// <inheritdoc />
+    public partial class createpeopletable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "People",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Password = table.Column<string>(type: "text", nullable: false),
+                    Document = table.Column<string>(type: "text", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_People", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "People");
+        }
+    }
+}
