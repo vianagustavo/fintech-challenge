450723b67325d57cecc047eb09d0799e6c6503dd
Gustavo <gustavo.ferreira@kinvo.com.br>
2023-09-18 16:49:24 -0300

    fix: add test env variables

--- fintech-challenge.Tests/People/PeopleControllerTests.cs
+++ fintech-challenge.Tests/People/PeopleControllerTests.cs
@@ -21,31 +21,25 @@ namespace FintechChallenge.Tests.Integration
         [Fact]
         public async Task CreatePeople_ValidRequest_ReturnsCreated()
         {
-            // Arrange
-            var createRequest = new CreatePeopleRequest("John Doe", "P@ssw0rd", "1234567890");
+            var createRequest = new CreatePeopleRequest("Test Name", "1234", "1234567890");
 
 
             var jsonRequest = JsonSerializer.Serialize(createRequest);
             var content = new StringContent(jsonRequest, Encoding.UTF8, "application/json");
 
-            // Act
             var response = await _client.PostAsync("/people", content);
 
-            // Assert
             Assert.Equal(HttpStatusCode.Created, response.StatusCode);
 
-            // Deserialize the response content to check the result
             var jsonResponse = await response.Content.ReadAsStringAsync();
             var createPeopleResponse = JsonSerializer.Deserialize<CreatePeopleResponse>(jsonResponse, new JsonSerializerOptions
             {
                 PropertyNameCaseInsensitive = true
             });
 
-            // Additional assertions based on the response
             Assert.NotNull(createPeopleResponse);
             Assert.NotEqual(Guid.Empty, createPeopleResponse.Id);
-            Assert.Equal("John Doe", createPeopleResponse.Name);
-            // Add more assertions as needed
+            Assert.Equal("Test Name", createPeopleResponse.Name);
         }
     }
 }
